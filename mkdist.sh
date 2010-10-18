695eb8df2f713e6d86affde7e5a0784d976c787c
lexa <lexa@bdec19c8-3f88-df11-976a-00e08134adaa>
2010-10-18 11:16:37 +0000

    re-commited
    git-svn-id: https://developer.libraw.org/svn/trunk/libraw@667 bdec19c8-3f88-df11-976a-00e08134adaa

---
+++ mkdist.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -d CVS ] ; then
+  echo 'Use mkdist script in cvs export-ed dirs'
+else
+  if [ -f Makefile.dist ] ; then
+    make regenerate  
+    rm -fr dcraw/*
+    rm -f dcraw/.gdbinit
+    cd dcraw
+    wget http://www.cybercom.net/~dcoffin/dcraw/dcraw.c
+    cd ..
+    rm -f internal/preprocess.pl clist2c.pl
+    rm doc/*.psd
+    mv Makefile.dist Makefile
+    rm DEVELOPER-NOTES
+    rm mkdist.sh
+  else
+   echo 'Wrong directory or mkdist.sh already executed'
+  fi
+fi
