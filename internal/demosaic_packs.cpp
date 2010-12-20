8f7344d0b7537e7e29ef49e690cbb94e0dbe1210
Alex Tutubalin <lexa@lexa.ru>
2010-12-20 20:20:35 +0300

    CFA denoise

--- internal/demosaic_packs.cpp
+++ internal/demosaic_packs.cpp
@@ -75,9 +75,9 @@ void CLASS refinement() {}
 // fallback to AHD and no correction
 void CLASS CA_correct_RT(float,float){}
 void CLASS amaze_demosaic_RT() { ahd_interpolate();}
-void CLASS green_equilibrate(float thresh) {} // modifJD
-void CLASS cfa_linedn(float linenoise){}//modifJD
-void CLASS cfa_impulse_gauss(float lclean, float cclean){}//modifJD
+void CLASS green_equilibrate(float thresh) {} 
+void CLASS cfa_linedn(float linenoise){}
+void CLASS cfa_impulse_gauss(float lclean, float cclean){}
 #endif
 
 #define sget4(s) sget4((uchar *)s)
