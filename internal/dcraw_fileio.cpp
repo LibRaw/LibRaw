06756101abca6879b5a27d3a335a7907345dd13c
Alex Tutubalin <lexa@lexa.ru>
2012-06-26 20:30:19 +0400

    dcraw 9.15 import, phase1

--- internal/dcraw_fileio.cpp
+++ internal/dcraw_fileio.cpp
@@ -153,7 +153,7 @@ void CLASS subtract (const char *fname)
   RUN_CALLBACK(LIBRAW_PROGRESS_DARK_FRAME,1,2);
 #endif
 }
-#line 9365 "dcraw/dcraw.c"
+#line 9539 "dcraw/dcraw.c"
 
 #ifndef NO_LCMS
 void CLASS apply_profile (const char *input, const char *output)
