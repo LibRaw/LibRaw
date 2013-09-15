aca379b108226a087adb18e7eeab279cfe113131
Alex Tutubalin <lexa@lexa.ru>
2013-09-16 03:13:17 +0400

    Preliminary Foveon support without GPLed dcraw code

--- internal/dcraw_fileio.cpp
+++ internal/dcraw_fileio.cpp
@@ -154,7 +154,7 @@ void CLASS subtract (const char *fname)
   RUN_CALLBACK(LIBRAW_PROGRESS_DARK_FRAME,1,2);
 #endif
 }
-#line 9944 "dcraw/dcraw.c"
+#line 9942 "dcraw/dcraw.c"
 #ifndef NO_LCMS
 void CLASS apply_profile (const char *input, const char *output)
 {
