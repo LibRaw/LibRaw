21dfc7d1cfb53e1d455b20ae189850c7f2daee1f
Alex Tutubalin <lexa@lexa.ru>
2013-11-10 21:04:39 +0400

    Nikon High-ISO iso values

--- dcraw/dcraw.c
+++ dcraw/dcraw.c
@@ -6073,6 +6073,8 @@ nf: order = 0x4949;
     tag |= uptag << 16;
     if (tag == 2 && strstr(make,"NIKON") && !iso_speed)
       iso_speed = (get2(),get2());
+    if (tag == 37 && strstr(make,"NIKON") && !iso_speed)
+      iso_speed = int(100.0 * pow(2.0,double(get2() >>8)/12.0-5.0));
     if (tag == 4 && len > 26 && len < 35) {
       if ((i=(get4(),get2())) != 0x7fff && !iso_speed)
 	iso_speed = 50 * pow (2.0, i/32.0 - 4);
