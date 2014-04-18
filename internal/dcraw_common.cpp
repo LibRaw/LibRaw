5de220d1c614d4fb6d6fb76aac04cdcb8136bc80
Alex Tutubalin <lexa@lexa.ru>
2014-04-18 12:36:59 +0400

    imacon full-load final

--- internal/dcraw_common.cpp
+++ internal/dcraw_common.cpp
@@ -1699,6 +1699,7 @@ void CLASS imacon_full_load_raw()
   int row, col;
 
   if (!image) return;
+
 #ifdef LIBRAW_LIBRARY_BUILD
   unsigned short *buf = (unsigned short *)malloc(width*3*sizeof(unsigned short));
   merror(buf,"imacon_full_load_raw");
@@ -1708,24 +1709,22 @@ void CLASS imacon_full_load_raw()
   {
 #ifdef LIBRAW_LIBRARY_BUILD
     checkCancel();
-#endif
-#ifdef LIBRAW_LIBRARY_BUILD
-	read_shorts(buf,width*3);
-	unsigned short (*rowp)[4] = &image[row*width];
-	for (col=0; col < width; col++)
-	{
-		rowp[col][0]=buf[col*3];
-		rowp[col][1]=buf[col*3+1];
-		rowp[col][2]=buf[col*3+2];
-		rowp[col][3]=0;
-	}
+    read_shorts(buf,width*3);
+    unsigned short (*rowp)[4] = &image[row*width];
+    for (col=0; col < width; col++)
+      {
+        rowp[col][0]=buf[col*3];
+        rowp[col][1]=buf[col*3+1];
+        rowp[col][2]=buf[col*3+2];
+        rowp[col][3]=0;
+      }
 #else
     for (col=0; col < width; col++)
       read_shorts (image[row*width+col], 3);
 #endif
   }
 #ifdef LIBRAW_LIBRARY_BUILD
-	free(buf);
+  free(buf);
 #endif
 }
 
