78d005973a3ac898f515ac9fb18335c574b7bdfd
Alex Tutubalin <lexa@lexa.ru>
2014-04-20 16:05:42 +0400

    imacon 4-color speedup part1

--- internal/dcraw_common.cpp
+++ internal/dcraw_common.cpp
@@ -1699,14 +1699,34 @@ void CLASS imacon_full_load_raw()
   int row, col;
 
   if (!image) return;
+#ifdef LIBRAW_LIBRARY_BUILD
+  unsigned short *buf = (unsigned short *)malloc(width*3*sizeof(unsigned short));
+  merror(buf,"imacon_full_load_raw");
+#endif
+
   for (row=0; row < height; row++)
   {
 #ifdef LIBRAW_LIBRARY_BUILD
     checkCancel();
 #endif
+#ifdef LIBRAW_LIBRARY_BUILD
+	read_shorts(buf,width*3);
+	unsigned short (*rowp)[4] = &image[row*width];
+	for (col=0; col < width; col++)
+	{
+		rowp[col][0]=buf[col*3];
+		rowp[col][1]=buf[col*3+1];
+		rowp[col][2]=buf[col*3+2];
+		rowp[col][3]=0;
+	}
+#else
     for (col=0; col < width; col++)
       read_shorts (image[row*width+col], 3);
+#endif
   }
+#ifdef LIBRAW_LIBRARY_BUILD
+	free(buf);
+#endif
 }
 
 void CLASS packed_load_raw()
