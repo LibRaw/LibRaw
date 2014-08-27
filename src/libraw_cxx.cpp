11b971f2b7423b48a810763ec125b106d2131222
Alex Tutubalin <lexa@lexa.ru>
2014-08-27 19:58:46 +0400

    moved intNN_t to head of libraw_cxx.cpp

--- src/libraw_cxx.cpp
+++ src/libraw_cxx.cpp
@@ -35,6 +35,26 @@ it under the terms of the one of three licenses as you choose:
 #include "libraw/libraw.h"
 #include "internal/defines.h"
 
+
+#if defined(_WIN32)
+#if defined _MSC_VER
+typedef   signed __int8   int8_t;
+typedef unsigned __int8   uint8_t;
+typedef   signed __int16  int16_t;
+typedef unsigned __int16  uint16_t;
+typedef   signed __int32  int32_t;
+typedef unsigned __int32  uint32_t;
+typedef   signed __int64  int64_t;
+typedef unsigned __int64  uint64_t;
+#else
+#include <stdint.h>
+#endif // _WIN32
+#include <sys/types.h>
+#else
+#include <inttypes.h>
+#endif
+
+
 #ifdef USE_RAWSPEED
 #include "../RawSpeed/rawspeed_xmldata.cpp"
 #include <RawSpeed/StdAfx.h>
@@ -183,6 +203,7 @@ int LibRaw::is_sraw() { return load_raw == &LibRaw::canon_sraw_load_raw || load_
 int LibRaw::is_nikon_sraw(){ return load_raw == &LibRaw::nikon_load_sraw;}
 int LibRaw::sraw_midpoint() {if (load_raw == &LibRaw::canon_sraw_load_raw) return 8192; else if (load_raw == &LibRaw::nikon_load_sraw) return 2048; else return 0;}
 
+
 #ifdef USE_RAWSPEED
 using namespace RawSpeed;
 class CameraMetaDataLR : public CameraMetaData
@@ -228,7 +249,7 @@ CameraMetaDataLR::CameraMetaDataLR(char *data, int sz) : CameraMetaData() {
       addCamera(camera);
       
       // Create cameras for aliases.
-      for (uint32_t i = 0; i < camera->aliases.size(); i++) {
+      for (unsigned int i = 0; i < camera->aliases.size(); i++) {
         addCamera(new Camera(camera, i));
       }
     }
