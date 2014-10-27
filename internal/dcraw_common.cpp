7fd5a31d3d71cb6ecfa47e35e12d6208e8440c51
Alex Tutubalin <lexa@lexa.ru>
2014-10-27 19:33:01 +0400

    imported in dcraw.c

--- internal/dcraw_common.cpp
+++ internal/dcraw_common.cpp
@@ -127,14 +127,14 @@ double CLASS getreal (int type)
     case 3: return (unsigned short) get2();
     case 4: return (unsigned int) get4();
     case 5:  
-		u.d = (unsigned int) get4();
-		v.d = (unsigned int)get4();
-		return u.d / (v.d ? v.d : 1);
+      u.d = (unsigned int) get4();
+      v.d = (unsigned int)get4();
+      return u.d / (v.d ? v.d : 1);
     case 8: return (signed short) get2();
     case 9: return (signed int) get4();
     case 10: 
-		u.d = (signed int) get4();
-		v.d = (signed int)get4();
+      u.d = (signed int) get4();
+      v.d = (signed int)get4();
       return u.d / (v.d?v.d:1);
     case 11: return int_to_float (get4());
     case 12:
