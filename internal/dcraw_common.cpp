1746a10c93bf8bff5f484104e46d5ddbbd40023a
Alex Tutubalin <lexa@lexa.ru>
2014-10-27 18:31:20 +0300

    getreal division by zero

--- internal/dcraw_common.cpp
+++ internal/dcraw_common.cpp
@@ -120,18 +120,22 @@ float CLASS int_to_float (int i)
 
 double CLASS getreal (int type)
 {
-  union { char c[8]; double d; } u;
+  union { char c[8]; double d; } u,v;
   int i, rev;
 
   switch (type) {
     case 3: return (unsigned short) get2();
     case 4: return (unsigned int) get4();
-    case 5:  u.d = (unsigned int) get4();
-      return u.d / (unsigned int) get4();
+    case 5:  
+		u.d = (unsigned int) get4();
+		v.d = (unsigned int)get4();
+		return u.d / (v.d ? v.d : 1);
     case 8: return (signed short) get2();
     case 9: return (signed int) get4();
-    case 10: u.d = (signed int) get4();
-      return u.d / (signed int) get4();
+    case 10: 
+		u.d = (signed int) get4();
+		v.d = (signed int)get4();
+      return u.d / (v.d?v.d:1);
     case 11: return int_to_float (get4());
     case 12:
       rev = 7 * ((order == 0x4949) == (ntohs(0x1234) == 0x1234));
