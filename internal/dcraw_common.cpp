73897faf106e61b474bf165cd2e8cb972eb7bbde
Alex Tutubalin <lexa@lexa.ru>
2014-12-25 12:19:07 +0300

    Koda thumbs

--- internal/dcraw_common.cpp
+++ internal/dcraw_common.cpp
@@ -2876,7 +2876,7 @@ void CLASS kodak_ycbcr_load_raw()
 void CLASS kodak_rgb_load_raw()
 {
   short buf[768], *bp;
-  int row, col, len, c, i, rgb[3];
+  int row, col, len, c, i, rgb[3],ret;
   ushort *ip=image[0];
 
   for (row=0; row < height; row++)
@@ -2886,17 +2886,17 @@ void CLASS kodak_rgb_load_raw()
 #endif
     for (col=0; col < width; col+=256) {
       len = MIN (256, width-col);
-      kodak_65000_decode (buf, len*3);
+      ret = kodak_65000_decode (buf, len*3);
       memset (rgb, 0, sizeof rgb);
       for (bp=buf, i=0; i < len; i++, ip+=4)
 #ifdef LIBRAW_LIBRARY_BUILD
         if(load_flags == 12)
           {
-            FORC3 ip[c] = (rgb[c] += *bp++) &0xfff;
+			  FORC3 ip[c] = ret ? (*bp++) : (rgb[c] += *bp++);
           }
         else
 #endif
-          FORC3 if ((ip[c] = rgb[c] += *bp++) >> 12) derror();
+		FORC3 if ((ip[c] = ret ? (*bp++) : (rgb[c] += *bp++)) >> 12) derror();
     }
   }
 }
