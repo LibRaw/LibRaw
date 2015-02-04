8779a52916440d255b082909eb95f22a2c1b83fe
Alex Tutubalin <lexa@lexa.ru>
2015-02-04 14:49:32 +0300

    Oly EM5-2 support

--- src/libraw_cxx.cpp
+++ src/libraw_cxx.cpp
@@ -1366,6 +1366,9 @@ int LibRaw::unpack(void)
 	int rawspeed_enabled = 1;
 	if(imgdata.idata.dng_version && libraw_internal_data.unpacker_data.tiff_samples == 2)
 		rawspeed_enabled = 0;
+	// Disable rawspeed for double-sized Oly files
+	if(!strncasecmp(imgdata.idata.make,"Olympus",7) && !strncasecmp(imgdata.idata.model,"E-M5MarkII",10) && imgdata.sizes.raw_width == 9280)
+		rawspeed_enabled = 0;
     // RawSpeed Supported, 
     if(O.use_rawspeed  && rawspeed_enabled
        && !(is_sraw() && O.sraw_ycc)
