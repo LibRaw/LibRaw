6cc3c8c37b6ba5d903554bdb0da1a8c5e1440ae9
Alex Tutubalin <lexa@lexa.ru>
2015-05-08 15:05:13 +0300

    Changelog for Alpha2,  shlib version increment

--- libraw/libraw_version.h
+++ libraw/libraw_version.h
@@ -26,9 +26,9 @@ it under the terms of the one of three licenses as you choose:
 #define LIBRAW_MAJOR_VERSION  0
 #define LIBRAW_MINOR_VERSION  17
 #define LIBRAW_PATCH_VERSION  0
-#define LIBRAW_VERSION_TAIL   Alpha1
+#define LIBRAW_VERSION_TAIL   Alpha2
 
-#define LIBRAW_SHLIB_CURRENT  	11
+#define LIBRAW_SHLIB_CURRENT  	12
 #define LIBRAW_SHLIB_REVISION 	0
 #define LIBRAW_SHLIB_AGE     	0
 
