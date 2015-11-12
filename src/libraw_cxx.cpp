b916ad3345e36ed191e549ac9b8555ccb478b194
Alex Tutubalin <lexa@lexa.ru>
2015-11-12 16:54:32 +0300

    Nikon Exxxx: rawspeed disabled

--- src/libraw_cxx.cpp
+++ src/libraw_cxx.cpp
@@ -2242,6 +2242,9 @@ int LibRaw::unpack(void)
 			&& (load_raw == &LibRaw::canon_sraw_load_raw))
 			rawspeed_enabled = 0;
 
+		if(load_raw == &LibRaw::packed_load_raw && !strncasecmp(imgdata.idata.make,"Nikon",5) && !strncasecmp(imgdata.idata.model,"E",1) )
+			rawspeed_enabled = 0;
+
 		// RawSpeed Supported,
 		if(O.use_rawspeed  && rawspeed_enabled
 			&& !(is_sraw() && O.sraw_ycc)
