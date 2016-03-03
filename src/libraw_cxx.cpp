51cf4fd2667a56a3e7e91897de7f43c7ab2e9d00
Alex Tutubalin <lexa@lexa.ru>
2016-03-03 23:09:03 +0300

    note about Pro2

--- src/libraw_cxx.cpp
+++ src/libraw_cxx.cpp
@@ -4446,7 +4446,7 @@ static const char  *static_camera_list[] =
 "FujiFilm F800EXR",
 "FujiFilm F900EXR",
 "FujiFilm X-Pro1",
-"FujiFilm X-Pro2",
+"FujiFilm X-Pro2 (uncompressed RAW only)",
 "FujiFilm X-S1",
 "FujiFilm XQ1",
 "FujiFilm XQ2",
