37639562e2905250cf981ed20eed192039562c57
Alex Tutubalin <lexa@lexa.ru>
2016-05-19 19:06:12 +0300

    skip zeroes on trailing space

--- internal/dcraw_common.cpp
+++ internal/dcraw_common.cpp
@@ -12978,11 +12978,10 @@ static void remove_trailing_spaces(char *string, size_t len)
   if(len<1) return; // not needed, b/c sizeof of make/model is 64
   string[len-1]=0;
   if(len<3) return; // also not needed
-  for(int i=len-2; i>=0; i--)
+  len = strlen(string);
+  for(int i=len-1; i>=0; i--)
   {
-    if(string[i] == 0)
-      continue;
-    else if(isspace(string[i]))
+    if(isspace(string[i]))
       string[i]=0;
     else
       break;
