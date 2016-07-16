765def23f0675fe400e34b232feb2c87ccba7841
Alex Tutubalin <lexa@lexa.ru>
2016-07-16 14:38:33 +0300

    Revert "auto_ptr -> unique_ptr and std::move"
    This reverts commit 7a27e13c2d3aa2f16a7928021092c050f1c5be15.
    Not compatible with gcc4

--- src/libraw_datastream.cpp
+++ src/libraw_datastream.cpp
@@ -82,10 +82,10 @@ LibRaw_file_datastream::LibRaw_file_datastream(const char *fname)
         _fsize = st.st_size;
 #endif
       
-      std::unique_ptr<std::filebuf> buf(new std::filebuf());
+      std::auto_ptr<std::filebuf> buf(new std::filebuf());
       buf->open(filename.c_str(), std::ios_base::in | std::ios_base::binary);
       if (buf->is_open()) {
-        f = std::move(buf);
+        f = buf;
       }
     }
 }
@@ -97,10 +97,10 @@ LibRaw_file_datastream::LibRaw_file_datastream(const wchar_t *fname) : filename(
       struct _stati64 st;
       if(!_wstati64(wfilename.c_str(),&st))
         _fsize = st.st_size;
-      std::unique_ptr<std::filebuf> buf(new std::filebuf());
+      std::auto_ptr<std::filebuf> buf(new std::filebuf());
       buf->open(wfilename.c_str(), std::ios_base::in | std::ios_base::binary);
       if (buf->is_open()) {
-        f = std::move(buf);
+        f = buf;
       }
     }
 }
@@ -199,19 +199,18 @@ int LibRaw_file_datastream::subfile_open(const char *fn)
 {
     LR_STREAM_CHK();
     if (saved_f.get()) return EBUSY;
-    saved_f = std::move(f);
+    saved_f = f;
+        std::auto_ptr<std::filebuf> buf(new std::filebuf());
         
-	std::unique_ptr<std::filebuf> buf(new std::filebuf());
+        buf->open(fn, std::ios_base::in | std::ios_base::binary);
+        if (!buf->is_open()) {
+            f = saved_f;
+            return ENOENT;
+        } else {
+            f = buf;
+        }
         
-	buf->open(fn, std::ios_base::in | std::ios_base::binary);
-    if (!buf->is_open()) 
-	{
-		f = std::move(saved_f);
-        return ENOENT;
-	} else {
-        f = std::move(buf);
-    }    
-    return 0;
+        return 0;
 }
 
 #if defined(_WIN32) && !defined(__MINGW32__) && defined(_MSC_VER) && (_MSC_VER > 1310)
@@ -219,16 +218,17 @@ int LibRaw_file_datastream::subfile_open(const wchar_t *fn)
 {
 	LR_STREAM_CHK();
 	if (saved_f.get()) return EBUSY;
-	saved_f = std::move(f);
-	std::unique_ptr<std::filebuf> buf(new std::filebuf());
+	saved_f = f;
+	std::auto_ptr<std::filebuf> buf(new std::filebuf());
 
 	buf->open(fn, std::ios_base::in | std::ios_base::binary);
 	if (!buf->is_open()) {
-		f =  std::move(saved_f);
+		f = saved_f;
 		return ENOENT;
 	} else {
-		f =  std::move(buf);
+		f = buf;
 	}
+
 	return 0;
 }
 #endif
@@ -237,7 +237,7 @@ int LibRaw_file_datastream::subfile_open(const wchar_t *fn)
 void LibRaw_file_datastream::subfile_close()
 { 
     if (!saved_f.get()) return; 
-    f = std::move(saved_f);   
+    f = saved_f;   
 }
 
 #undef LR_STREAM_CHK
