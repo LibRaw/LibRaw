6f0e1ba89255420d9230b8f967241a35dc37357e
Iliah Borg <iliah.i.borg@gmail.com>
2016-10-13 13:22:06 -0400

    FujiMakernotes: some tags added

--- internal/dcraw_common.cpp
+++ internal/dcraw_common.cpp
@@ -6643,12 +6643,16 @@ void CLASS setPhaseOneFeatures (unsigned id) {
 
 void CLASS parseFujiMakernotes (unsigned tag, unsigned type) {
       switch (tag) {
+      case 0x1002: imgdata.makernotes.fuji.WB_Preset = get2(); break;
       case 0x1011: imgdata.other.FlashEC = getreal(type); break;
+      case 0x1020: imgdata.makernotes.fuji.Macro = get2(); break;
       case 0x1021: imgdata.makernotes.fuji.FocusMode = get2(); break;
       case 0x1022: imgdata.makernotes.fuji.AFMode = get2(); break;
       case 0x1023: imgdata.makernotes.fuji.FocusPixel[0] = get2();
                    imgdata.makernotes.fuji.FocusPixel[1] = get2();
       break;
+      case 0x1034: imgdata.makernotes.fuji.ExrMode = get2(); break;
+      case 0x1050: imgdata.makernotes.fuji.ShutterType = get2(); break;
       case 0x1400: imgdata.makernotes.fuji.FujiDynamicRange = get2(); break;
       case 0x1401: imgdata.makernotes.fuji.FujiFilmMode = get2(); break;
       case 0x1402: imgdata.makernotes.fuji.FujiDynamicRangeSetting = get2(); break;
@@ -6663,6 +6667,10 @@ void CLASS parseFujiMakernotes (unsigned tag, unsigned type) {
                    imgdata.makernotes.fuji.ImageStabilization[2] = get2();
                    imgdata.shootinginfo.ImageStabilization = (imgdata.makernotes.fuji.ImageStabilization[0]<<9) + imgdata.makernotes.fuji.ImageStabilization[1];
       break;
+      case 0x1431: imgdata.makernotes.fuji.Rating = get4(); break;
+      case 0x3820: imgdata.makernotes.fuji.FrameRate = get2(); break;
+      case 0x3821: imgdata.makernotes.fuji.FrameWidth = get2(); break;
+      case 0x3822: imgdata.makernotes.fuji.FrameHeight = get2(); break;
       }
 return;
 }
