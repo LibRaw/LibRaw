2bbc6d410bb87d349629452cfeb93d4b40518d4c
Alex Tutubalin <lexa@lexa.ru>
2016-10-31 11:36:24 +0300

    xtrans openmp patch

--- src/libraw_xtrans_compressed.cpp
+++ src/libraw_xtrans_compressed.cpp
@@ -738,10 +738,11 @@ void LibRaw::xtrans_compressed_load_raw()
 
 void LibRaw::xtrans_decode_loop(const struct xtrans_params* common_info, int count, INT64* raw_block_offsets, unsigned *block_sizes)
 {
+	int cur_block;
 #ifdef LIBRAW_USE_OPENMP
 #pragma omp parallel for private(cur_block)
 #endif
-	for (int cur_block = 0; cur_block < count ; cur_block++)
+	for (cur_block = 0; cur_block < count ; cur_block++)
 	{
 		xtrans_decode_strip(common_info, cur_block, raw_block_offsets[cur_block], block_sizes[cur_block]);
 	}
