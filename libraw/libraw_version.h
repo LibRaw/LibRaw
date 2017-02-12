8fb36dc6876cdcc3c39c510fea6260a2ffe78afb
Alex Tutubalin <lexa@lexa.ru>
2017-02-12 09:23:28 +0300

    Version increment

--- libraw/libraw_version.h
+++ libraw/libraw_version.h
@@ -21,11 +21,11 @@ it under the terms of the one of two licenses as you choose:
 #define __VERSION_H
 
 #define LIBRAW_MAJOR_VERSION 0
-#define LIBRAW_MINOR_VERSION 18
+#define LIBRAW_MINOR_VERSION 19
 #define LIBRAW_PATCH_VERSION 0
-#define LIBRAW_VERSION_TAIL Release
+#define LIBRAW_VERSION_TAIL Snapshot-20170212
 
-#define LIBRAW_SHLIB_CURRENT 16
+#define LIBRAW_SHLIB_CURRENT 17
 #define LIBRAW_SHLIB_REVISION 0
 #define LIBRAW_SHLIB_AGE 0
 
