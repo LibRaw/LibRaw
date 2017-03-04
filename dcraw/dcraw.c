e404386278c37e887302ecc3f817dc7d3723671a
Alex Tutubalin <lexa@lexa.ru>
2017-03-04 21:25:03 +0300

    Secunia SA75000 advisory: several buffer overruns

--- dcraw/dcraw.c
+++ dcraw/dcraw.c
@@ -11697,7 +11697,12 @@ int CLASS parse_tiff_ifd (int base)
 	if (!strcmp(model,"DSLR-A100") && tiff_ifd[ifd].t_width == 3872) {
 	  load_raw = &CLASS sony_arw_load_raw;
 	  data_offset = get4()+base;
-	  ifd++;  break;
+	  ifd++;
+#ifdef LIBRAW_LIBRARY_BUILD
+          if (ifd >= sizeof tiff_ifd / sizeof tiff_ifd[0])
+         	throw LIBRAW_EXCEPTION_IO_CORRUPT;
+#endif  
+	  break;
 	}
 #ifdef LIBRAW_LIBRARY_BUILD
 	if (!strncmp(make,"Hasselblad",10) && libraw_internal_data.unpacker_data.hasselblad_parser_flag) {
@@ -11965,7 +11970,7 @@ int CLASS parse_tiff_ifd (int base)
 	break;
       case 50454:			/* Sinar tag */
       case 50455:
-	if (len > 2560000 || !(cbuf = (char *) malloc(len))) break;
+	if (len < 1 || len > 2560000 || !(cbuf = (char *) malloc(len))) break;
 #ifndef LIBRAW_LIBRARY_BUILD
 	fread (cbuf, 1, len, ifp);
 #else
@@ -13326,7 +13331,11 @@ int CLASS parse_jpeg (int offset)
     }
     order = get2();
     hlen  = get4();
-    if (get4() == 0x48454150)		/* "HEAP" */
+    if (get4() == 0x48454150
+#ifdef LIBRAW_LIBRARY_BUILD
+        && (save+hlen) >= 0 && (save+hlen)<=ifp->size()
+#endif
+    )		/* "HEAP" */
     {
 #ifdef LIBRAW_LIBRARY_BUILD
       imgdata.lens.makernotes.CameraMount = LIBRAW_MOUNT_FixedLens;
