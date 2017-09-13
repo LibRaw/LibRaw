d13e8f6d1e987b7491182040a188c16a395f1d21
Alex Tutubalin <lexa@lexa.ru>
2017-09-13 09:31:01 +0300

    CVE-2017-1438 credits; fix for Kodak 65000 out of bounds access

--- dcraw/dcraw.c
+++ dcraw/dcraw.c
@@ -3528,8 +3528,15 @@ void CLASS kodak_65000_load_raw()
       len = MIN (256, width-col);
       ret = kodak_65000_decode (buf, len);
       for (i=0; i < len; i++)
-	if ((RAW(row,col+i) =	curve[ret ? buf[i] :
-		(pred[i & 1] += buf[i])]) >> 12) derror();
+      {
+	int idx = ret ? buf[i] : (pred[i & 1] += buf[i]);
+	if(idx >=0 && idx <= 0xffff)
+	 {
+	   if ((RAW(row,col+i) = curve[idx]) >> 12) derror();
+         }
+	 else
+	   derror();
+      }
     }
   }
 }
