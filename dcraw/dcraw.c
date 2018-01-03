5e415e04ace7f27012acfa45b772b93851fe93bc
Iliah Borg <iliah.i.borg@gmail.com>
2018-01-03 13:36:21 -0500

    SONY: added missing id to setSonyBodyFeatures

--- dcraw/dcraw.c
+++ dcraw/dcraw.c
@@ -9379,20 +9379,20 @@ void CLASS setSonyBodyFeatures(unsigned id)
   if (               // FF cameras
       (id == 257) || // a900
       (id == 269) || // a850
-      (id == 340) || // ILCE-7M2
-      (id == 318) || // ILCE-7S
-      (id == 350) || // ILCE-7SM2
-      (id == 311) || // ILCE-7R
-      (id == 347) || // ILCE-7RM2
-      (id == 306) || // ILCE-7
+      (id == 294) || // SLT-99, Hasselblad HV
       (id == 298) || // DSC-RX1
       (id == 299) || // NEX-VG900
+      (id == 306) || // ILCE-7
       (id == 310) || // DSC-RX1R
+      (id == 311) || // ILCE-7R
+      (id == 318) || // ILCE-7S
+      (id == 340) || // ILCE-7M2
       (id == 344) || // DSC-RX1RM2
+      (id == 347) || // ILCE-7RM2
+      (id == 350) || // ILCE-7SM2
       (id == 354) || // ILCA-99M2
       (id == 358) || // ILCE-9
-      (id == 362) || // ILCE-7RM3
-      (id == 294)    // SLT-99, Hasselblad HV
+      (id == 362)    // ILCE-7RM3
   )
   {
     imgdata.lens.makernotes.CameraFormat = LIBRAW_FORMAT_FF;
@@ -9418,37 +9418,44 @@ void CLASS setSonyBodyFeatures(unsigned id)
   }
 
   if ( // E-mount cameras, ILCE series
-      (id == 302) || (id == 306) || (id == 311) || (id == 312) || (id == 313) || (id == 318) || (id == 339) ||
-      (id == 340) || (id == 346) || (id == 347) || (id == 350) || (id == 358) || (id == 360) || (id == 362))
+      (id == 302) || (id == 306) || (id == 311) || (id == 312) || (id == 313) || (id == 318) ||
+      (id == 339) || (id == 340) || (id == 346) || (id == 347) || (id == 350) || (id == 357) ||
+      (id == 358) || (id == 360) || (id == 362)
+     )
   {
     imgdata.lens.makernotes.CameraMount = LIBRAW_MOUNT_Sony_E;
     imgdata.makernotes.sony.SonyCameraType = LIBRAW_SONY_ILCE;
   }
   else if ( // E-mount cameras, NEX series
-      (id == 278) || (id == 279) || (id == 284) || (id == 288) || (id == 289) || (id == 290) || (id == 293) ||
-      (id == 295) || (id == 296) || (id == 299) || (id == 300) || (id == 305) || (id == 307))
+      (id == 278) || (id == 279) || (id == 284) || (id == 288) || (id == 289) || (id == 290) ||
+      (id == 293) || (id == 295) || (id == 296) || (id == 299) || (id == 300) || (id == 305) ||
+      (id == 307)
+     )
   {
     imgdata.lens.makernotes.CameraMount = LIBRAW_MOUNT_Sony_E;
     imgdata.makernotes.sony.SonyCameraType = LIBRAW_SONY_NEX;
   }
 
   else if ( // A-mount cameras, DSLR series
-      (id == 256) || (id == 257) || (id == 258) || (id == 259) || (id == 260) || (id == 261) || (id == 262) ||
-      (id == 263) || (id == 264) || (id == 265) || (id == 266) || (id == 269) || (id == 270) || (id == 273) ||
-      (id == 274) || (id == 275) || (id == 282) || (id == 283))
+      (id == 256) || (id == 257) || (id == 258) || (id == 259) || (id == 260) || (id == 261) ||
+      (id == 262) || (id == 263) || (id == 264) || (id == 265) || (id == 266) || (id == 269) ||
+      (id == 270) || (id == 273) || (id == 274) || (id == 275) || (id == 282) || (id == 283)
+     )
   {
     imgdata.lens.makernotes.CameraMount = LIBRAW_MOUNT_Minolta_A;
     imgdata.makernotes.sony.SonyCameraType = LIBRAW_SONY_DSLR;
   }
   else if ( // A-mount cameras, SLT series
-      (id == 280) || (id == 281) || (id == 285) || (id == 286) || (id == 287) || (id == 291) || (id == 292) ||
-      (id == 294) || (id == 303))
+      (id == 280) || (id == 281) || (id == 285) || (id == 286) || (id == 287) || (id == 291) ||
+      (id == 292) || (id == 294) || (id == 303)
+     )
   {
     imgdata.lens.makernotes.CameraMount = LIBRAW_MOUNT_Minolta_A;
     imgdata.makernotes.sony.SonyCameraType = LIBRAW_SONY_SLT;
   }
   else if ( // A-mount cameras, ILCA series
-      (id == 319) || (id == 353) || (id == 354))
+      (id == 319) || (id == 353) || (id == 354)
+     )
   {
     imgdata.lens.makernotes.CameraMount = LIBRAW_MOUNT_Minolta_A;
     imgdata.makernotes.sony.SonyCameraType = LIBRAW_SONY_ILCA;
@@ -9461,10 +9468,10 @@ void CLASS setSonyBodyFeatures(unsigned id)
       (id == 308) || // DSC-RX100M2
       (id == 309) || // DSC-RX10
       (id == 310) || // DSC-RX1R
-      (id == 344) || // DSC-RX1RM2
       (id == 317) || // DSC-RX100M3
       (id == 341) || // DSC-RX100M4
       (id == 342) || // DSC-RX10M2
+      (id == 344) || // DSC-RX1RM2
       (id == 355) || // DSC-RX10M3
       (id == 356) || // DSC-RX100M5
       (id == 364) || // DSC-RX0
