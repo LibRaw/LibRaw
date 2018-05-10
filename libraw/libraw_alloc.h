e0a8637ac037ae6e80ef9516059e0d3cded481f7
Alex Tutubalin <lexa@lexa.ru>
2018-05-10 09:48:36 +0300

    define LIBRAW_USE_CALLOC_INSTEAD_OF_MALLOC to use ::calloc instead of ::malloc in LibRaw_mem_mgr malloc calls; Note: realloc is not changed, so only partial fix

--- libraw/libraw_alloc.h
+++ libraw/libraw_alloc.h
@@ -48,7 +48,11 @@ public:
     if(alloc_cnt >= LIBRAW_MSIZE)
        throw LIBRAW_EXCEPTION_ALLOC;
 #endif
+#ifdef LIBRAW_USE_CALLOC_INSTEAD_OF_MALLOC
+    void *ptr = ::calloc(sz + extra_bytes,1);
+#else
     void *ptr = ::malloc(sz + extra_bytes);
+#endif
     mem_ptr(ptr);
     return ptr;
   }
