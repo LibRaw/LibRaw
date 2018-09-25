9a47ee5c29a56bee2a01e215e259ef5d4301c03d
Alex Tutubalin <lexa@lexa.ru>
2018-09-25 19:08:08 +0300

    Coolpix P1000: disable RawSpeed processing

--- src/libraw_cxx.cpp
+++ src/libraw_cxx.cpp
@@ -2937,7 +2937,7 @@ int LibRaw::unpack(void)
         rawspeed_enabled = 0;
 
       if (load_raw == &LibRaw::packed_load_raw && !strncasecmp(imgdata.idata.make, "Nikon", 5) &&
-          (!strncasecmp(imgdata.idata.model, "E", 1) || !strncasecmp(imgdata.idata.model, "COOLPIX B", 9)))
+          (!strncasecmp(imgdata.idata.model, "E", 1) || !strncasecmp(imgdata.idata.model, "COOLPIX B", 9) ||!!strncasecmp(imgdata.idata.model, "COOLPIX P1000", 13)))
         rawspeed_enabled = 0;
 
       // RawSpeed Supported,
