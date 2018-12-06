64f4c69eb353dd5367c066506f6a0d2dbd747027
Alex Tutubalin <lexa@lexa.ru>
2018-12-06 12:27:10 +0300

    remove_zeroes: fix for 1st two columns/rows

--- dcraw/dcraw.c
+++ dcraw/dcraw.c
@@ -5909,7 +5909,8 @@ mask_set:
 
 void CLASS remove_zeroes()
 {
-  unsigned row, col, tot, n, r, c;
+  unsigned row, col, tot, n;
+  int r, c;
 
 #ifdef LIBRAW_LIBRARY_BUILD
   RUN_CALLBACK(LIBRAW_PROGRESS_REMOVE_ZEROES, 0, 2);
@@ -5920,9 +5921,9 @@ void CLASS remove_zeroes()
       if (BAYER(row, col) == 0)
       {
         tot = n = 0;
-        for (r = row - 2; r <= row + 2; r++)
-          for (c = col - 2; c <= col + 2; c++)
-            if (r < height && c < width && FC(r, c) == FC(row, col) && BAYER(r, c))
+        for (r = (int)row - 2; r <= (int)row + 2; r++)
+          for (c = (int)col - 2; c <= (int)col + 2; c++)
+            if (r >=0 && r < height && c >= 0 && c < width && FC(r, c) == FC(row, col) && BAYER(r, c))
               tot += (n++, BAYER(r, c));
         if (n)
           BAYER(row, col) = tot / n;
