1e0a05a154ddee7f6b5070a7f6ae7f08bc983021
Alex Tutubalin <lexa@lexa.ru>
2019-01-11 13:03:30 +0300

    better mempool overflow handling

--- libraw/libraw_alloc.h
+++ libraw/libraw_alloc.h
@@ -86,13 +86,15 @@ private:
   {
     if (ptr)
     {
-      for (int i = 0; i < LIBRAW_MSIZE; i++)
+      for (int i = 0; i < LIBRAW_MSIZE-1; i++)
         if (!mems[i])
         {
           mems[i] = ptr;
           return;
         }
 #ifdef LIBRAW_MEMPOOL_CHECK
+      /* remember ptr in last mems item to be free'ed at cleanup */
+      if(!mems[LIBRAW_MSIZE-1]) mems[LIBRAW_MSIZE-1] = ptr;
       throw LIBRAW_EXCEPTION_MEMPOOL;
 #endif
     }
