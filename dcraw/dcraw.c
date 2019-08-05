1375b07e467d7f7fda0c829471d9b2c8f9ef68ec
Alex Tutubalin <lexa@lexa.ru>
2019-08-05 18:35:58 +0300

    parseSonySRF: buffer size check

--- dcraw/dcraw.c
+++ dcraw/dcraw.c
@@ -13015,7 +13015,11 @@ void CLASS parseSonySRF (unsigned len)
   fread (srf_buf, len, 1, ifp);
 
   offset += srf_buf[offset] << 2;
-  if ((len-3) < offset) goto restore_after_parseSonySRF;
+
+#define CHECKBUFFER_SGET4(offset) do{ if((offset+4)>len) goto restore_after_parseSonySRF; }while(0)
+#define CHECKBUFFER_SGET2(offset) do{ if((offset+2)>len) goto restore_after_parseSonySRF; }while(0)
+
+  CHECKBUFFER_SGET4(offset);
 
 /* master key is stored in big endian */
   MasterKey = ((unsigned)srf_buf[offset]   << 24) |
@@ -13026,18 +13030,23 @@ void CLASS parseSonySRF (unsigned len)
 
 /* skip SRF0 */
   srf_offset = 0;
+  CHECKBUFFER_SGET2(srf_offset);
   entries = sget2(srf_buf+srf_offset);
   if (entries > 1000) goto restore_after_parseSonySRF;
   offset = srf_offset + 2;
+  CHECKBUFFER_SGET4(offset+12*entries);
   srf_offset = sget4(srf_buf + offset + 12*entries) - save; /* SRF0 ends with SRF1 abs. position */
 
 /* get SRF1, it has fixed 40 bytes length and contains keys to decode metadata and raw data */
+  /* Need to check len here too */
   sony_decrypt((unsigned *)(srf_buf+srf_offset), decrypt_len - srf_offset / 4, 1, MasterKey);
+  CHECKBUFFER_SGET2(srf_offset);
   entries = sget2(srf_buf+srf_offset);
   if (entries > 1000) goto restore_after_parseSonySRF;
   offset = srf_offset + 2;
 
   while (entries--) {
+    CHECKBUFFER_SGET4(srf_offset+8);
     tag_id = sget2(srf_buf+offset);
     tag_type = sget2(srf_buf+offset+2);
     tag_datalen = sget4(srf_buf+offset+4);
@@ -13048,12 +13057,15 @@ void CLASS parseSonySRF (unsigned len)
   }
 
 /* get SRF2 */
+  CHECKBUFFER_SGET4(offset);
   srf_offset = sget4(srf_buf+offset) - save; /* SRFn ends with SRFn+1 position */
   sony_decrypt((unsigned *)(srf_buf+srf_offset), decrypt_len - srf_offset / 4, 1, SRF2Key);
+  CHECKBUFFER_SGET2(srf_offset);
   entries = sget2(srf_buf+srf_offset);
   if (entries > 1000) goto restore_after_parseSonySRF;
   offset = srf_offset + 2;
   while (entries--) {
+    CHECKBUFFER_SGET4(srf_offset+8);
     tag_id = sget2(srf_buf+offset);
     tag_type = sget2(srf_buf+offset+2);
     tag_datalen = sget4(srf_buf+offset+4);
@@ -13066,18 +13078,23 @@ void CLASS parseSonySRF (unsigned len)
 0x0005	RawDataLength
 */
     case 0x0043:
+      CHECKBUFFER_SGET4(tag_val-save+4);
       i = sget4(srf_buf+tag_val-save+4);
+      // next sget4 already checked b/c offset is less
       if (i) ilm.MaxAp4MaxFocal  = ((float)sget4(srf_buf+tag_val-save)) / ((float)i);
       break;
     case 0x0044:
+      CHECKBUFFER_SGET4(tag_val-save+4);
       i = sget4(srf_buf+tag_val-save+4);
       if (i) ilm.MaxAp4MinFocal  = ((float)sget4(srf_buf+tag_val-save)) / ((float)i);
       break;
     case 0x0045:
+      CHECKBUFFER_SGET4(tag_val-save+4);
       i = sget4(srf_buf+tag_val-save+4);
       if (i) ilm.MinFocal = ((float)sget4(srf_buf+tag_val-save)) / ((float)i);
       break;
     case 0x0046:
+      CHECKBUFFER_SGET4(tag_val-save+4);
       i = sget4(srf_buf+tag_val-save+4);
       if (i) ilm.MaxFocal = ((float)sget4(srf_buf+tag_val-save)) / ((float)i);
       break;
@@ -13143,6 +13160,8 @@ void CLASS parseSonySRF (unsigned len)
 restore_after_parseSonySRF:
   free (srf_buf);
   fseek (ifp, save, SEEK_SET);
+#undef CHECKBUFFER_SGET4
+#undef CHECKBUFFER_SGET2
 }
 
 #undef ilm
