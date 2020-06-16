6303775f2a627194dd30eecca524e31b5e712a95
Jamie Pinheiro <pinheirojamie@google.com>
2020-06-16 17:49:49 +0000

    Fix bug

--- src/decoders/decoders_dcraw.cpp
+++ src/decoders/decoders_dcraw.cpp
@@ -370,9 +370,7 @@ int LibRaw::ljpeg_diff(ushort *huff)
     return -32768;
   diff = getbits(len);
 
-  len = MAX(1, len);
-
-  if ((diff & (1 << (len - 1))) == 0)
+  if ((diff & (len > 0 ? (1 << (len - 1)) : 0)) == 0)
     diff -= (1 << len) - 1;
   return diff;
 }
