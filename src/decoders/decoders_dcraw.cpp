cf82305e5c6d43b3c1054db4256b996375eaa735
Alex Tutubalin <lexa@lexa.ru>
2020-06-16 21:12:52 +0300

    rolled back Issue 23405 and 23309

--- src/decoders/decoders_dcraw.cpp
+++ src/decoders/decoders_dcraw.cpp
@@ -370,7 +370,6 @@ int LibRaw::ljpeg_diff(ushort *huff)
     return -32768;
   diff = getbits(len);
 
-  len = MAX(1, len);
 
   if ((diff & (1 << (len - 1))) == 0)
     diff -= (1 << len) - 1;
@@ -807,7 +806,7 @@ void LibRaw::pentax_load_raw()
   FORC(dep) bit[0][c] = get2();
   FORC(dep) bit[1][c] = fgetc(ifp);
   FORC(dep)
-  for (i = bit[0][c]; i <= ((bit[0][c] + (4096 >> LIM(bit[1][c], 0, 31)) - 1) & 4095);)
+  for (i = bit[0][c]; i <= ((bit[0][c] + (4096 >> bit[1][c]) - 1) & 4095);)
     huff[++i] = bit[1][c] << 8 | c;
   huff[0] = 12;
   fseek(ifp, data_offset, SEEK_SET);
