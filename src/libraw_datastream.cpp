e73a40d1dc33c009b0bc6b55ca3445e746c15e5a
Paolo Barbolini <paolo@paolo565.org>
2020-09-29 09:34:00 +0200

    Use a const void *buffer in 'open_buffer'

--- src/libraw_datastream.cpp
+++ src/libraw_datastream.cpp
@@ -348,7 +348,7 @@ void *LibRaw_file_datastream::make_jas_stream()
 }
 
 // == LibRaw_buffer_datastream
-LibRaw_buffer_datastream::LibRaw_buffer_datastream(void *buffer, size_t bsize)
+LibRaw_buffer_datastream::LibRaw_buffer_datastream(const void *buffer, size_t bsize)
 {
   buf = (unsigned char *)buffer;
   streampos = 0;
