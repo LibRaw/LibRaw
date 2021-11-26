30595a731f3bea78f0410426b73ef3afbd3f64c7
Alex Tutubalin <lexa@lexa.ru>
2021-11-26 18:31:45 +0300

    fixed wrong 32bit size_t conversion to INT64; libraw_open_wfile() compiles if LIBRAW_WIN32_UNICODEPATHS defined

--- src/libraw_c_api.cpp
+++ src/libraw_c_api.cpp
@@ -105,8 +105,7 @@ extern "C"
   }
 #endif
 
-#if defined(_WIN32) && !defined(__MINGW32__) && defined(_MSC_VER) &&           \
-    (_MSC_VER > 1310)
+#ifdef LIBRAW_WIN32_UNICODEPATHS
   int libraw_open_wfile(libraw_data_t *lr, const wchar_t *file)
   {
     if (!lr)
--- src/metadata/misc_parsers.cpp
+++ src/metadata/misc_parsers.cpp
@@ -71,7 +71,7 @@ int LibRaw::minolta_z2()
   int i, nz;
   char tail[424];
 
-  fseek(ifp, -sizeof tail, SEEK_END);
+  fseek(ifp, -int(sizeof tail), SEEK_END);
   fread(tail, 1, sizeof tail, ifp);
   for (nz = i = 0; i < int(sizeof tail); i++)
     if (tail[i])
