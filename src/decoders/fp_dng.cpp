a5376701e4965cae652a5460269b79d769cf2343
Alex Tutubalin <lexa@lexa.ru>
2022-10-27 13:42:54 +0300

    Apply memory limit to uncompressed_fp_dng_load_raw

--- src/decoders/fp_dng.cpp
+++ src/decoders/fp_dng.cpp
@@ -608,6 +608,10 @@ void LibRaw::uncompressed_fp_dng_load_raw()
     tiles.init(ifd, imgdata.sizes, libraw_internal_data.unpacker_data, libraw_internal_data.unpacker_data.order,
         libraw_internal_data.internal_data.input);
 
+	INT64 allocsz = INT64(tiles.tileCnt) * INT64(tiles.tileWidth) * INT64(tiles.tileHeight) * INT64(ifd->samples) * INT64(sizeof(float));
+	if (allocsz > INT64(imgdata.rawparams.max_raw_memory_mb) * INT64(1024 * 1024))
+		throw LIBRAW_EXCEPTION_TOOBIG;
+
     if (ifd->sample_format == 3)
         float_raw_image = (float *)calloc(tiles.tileCnt * tiles.tileWidth * tiles.tileHeight *ifd->samples, sizeof(float));
     else
