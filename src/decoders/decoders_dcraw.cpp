77a46b31978a2381e61df524dc9b6670a1f22321
Alex Tutubalin <lexa@lexa.ru>
2024-09-16 11:45:25 +0300

    panasonic loader: zero buf to avoid uninitalized data leak

--- src/decoders/decoders_dcraw.cpp
+++ src/decoders/decoders_dcraw.cpp
@@ -1063,7 +1063,10 @@ unsigned LibRaw::pana_data(int nb, unsigned *bytes)
   int byte;
 
   if (!nb && !bytes)
-    return vpos = 0;
+  {
+	  memset(buf, 0, sizeof(buf));
+	  return vpos = 0;
+  }
 
   if (!vpos)
   {
