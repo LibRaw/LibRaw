32836cf2286765c22bbb1e28326820c32a5e8981
Vincent Torri <vtorri@outlook.fr>
2025-04-14 11:02:02 +0200

    add meson build system

--- version.sh
+++ version.sh
@@ -6,7 +6,7 @@ major=`grep LIBRAW_MAJOR_VERSION $vfile |head -1 | awk '{print $3}'`
 minor=`grep LIBRAW_MINOR_VERSION $vfile | head -1 | awk '{print $3}'`
 patch=`grep LIBRAW_PATCH_VERSION $vfile | head -1 | awk '{print $3}'`
 tail=`grep LIBRAW_VERSION_TAIL $vfile | head -1 | awk '{print $3}'`
-
+tail="Release"
 if [ x$tail = xRelease ] ; then
  echo "$major.$minor.$patch" | awk '{printf $1}'
 else
